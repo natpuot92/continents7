1e8c59dead1a16109fee9bcb38c98cbef93022c4
natpuot92 <natpuot1992@gmail.com>
2017-08-02 00:07:07 +0300

    commit

--- app/initialize.js
+++ app/initialize.js
@@ -1,50 +1,13 @@
 import $ from "jquery"
-import slick from 'slick-carousel'
 
-function initSlick () {
-  $('.features-list').slick({
-    infinite: true,
-    arrows: false,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-    ]
-  })
-}
+var select = $('.main__form-select');
 
-$(document).ready(() => {
-  initSlick()
-})
+select.click(function() {
+  select.removeClass('select-color-white');
+  select.addClass('select-color-black');
+});
 
-
-/*(function () {
-var topLine = document.querySelector('.top-line');
-var pageHeader = document.querySelector('.page-header');
-var pageHeaderHeight = pageHeader.offsetHeight
-
-window.addEventListener('scroll', function() {
-  if (pageHeaderHeight > window.pageYOffset) {
-    topLine.classList.remove('sticky');
-  } else {
-    topLine.classList.add('sticky');
-  }
-})
-})(); */
-
+select.change(function() {
+  select.removeClass('select-color-black');
+  select.addClass('select-color-white');
+});
