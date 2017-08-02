fbe270082a77bb68952b2b83a893d8bd1f277d6f
natpuot92 <natpuot1992@gmail.com>
2017-08-02 13:52:03 +0300

    commit

--- app/initialize.js
+++ app/initialize.js
@@ -1,20 +1,6 @@
 import $ from "jquery"
 import validation from 'jquery-validation'
 
-var select = $('.main__form-select');
-
-select.click(function() {
-  select.removeClass('select-color-white');
-  select.addClass('select-color-black');
-});
-
-select.change(function() {
-  select.removeClass('select-color-black');
-  select.addClass('select-color-white');
-});
-
-
-
 $("#form").validate({
   rules: {
     first_name: {
