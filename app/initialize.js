f76d1f576cee8b08f067c0cab98fe06b2002cf07
natpuot92 <natpuot1992@gmail.com>
2017-08-02 13:01:15 +0300

    mobile

--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,5 @@
 import $ from "jquery"
+import validation from 'jquery-validation'
 
 var select = $('.main__form-select');
 
@@ -11,3 +12,26 @@ select.change(function() {
   select.removeClass('select-color-black');
   select.addClass('select-color-white');
 });
+
+
+
+$("#form").validate({
+  rules: {
+    first_name: {
+    required: true
+  },
+    last_name: {
+      required: true,
+    },
+    email: {
+    required: true,
+    email: true
+    },
+    country: {
+      required: true
+    }
+  },
+  errorPlacement: function(error, element) {
+    return true;
+  }
+})
