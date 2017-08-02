867399e50cb48a0bf3d283efead14a2cf757255b
natpuot92 <natpuot1992@gmail.com>
2017-08-02 16:52:52 +0300

    commit

--- app/initialize.js
+++ app/initialize.js
@@ -1,6 +1,14 @@
 import $ from "jquery"
 import validation from 'jquery-validation'
 
+$(document).ready(function(){
+  $("#worldmap").find("path").each(function(i, e){
+  $(e).css({
+'animationDuration': (Math.floor(Math.random()*2000)+2000)+'ms',
+'animationDelay': (Math.floor(Math.random()*2000))+'ms'
+})
+   })
+
 $("#form").validate({
   rules: {
     first_name: {
@@ -21,3 +29,6 @@ $("#form").validate({
     return true;
   }
 })
+})
+
+
