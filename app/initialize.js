f1563bca205c5fdaf793f436e3353fccf4e9599e
natpuot92 <natpuot1992@gmail.com>
2017-08-03 18:16:26 +0300

    commit

--- app/initialize.js
+++ app/initialize.js
@@ -1,13 +1,21 @@
 import $ from "jquery"
 import validation from 'jquery-validation'
+import countries from 'countries.js'
+import sweetalert from 'sweetalert'
+
+console.log(countries[1].name);
+
+for (var i = 0; countries.length > i; i++) {
+  $('.main__form-select').append('<option' + ' value =' + countries[i].name + '>' + countries[i].name + '</option>');
+}
 
 $(document).ready(function(){
   $("#worldmap").find("path").each(function(i, e){
   $(e).css({
-'animationDuration': (Math.floor(Math.random()*2000)+2000)+'ms',
-'animationDelay': (Math.floor(Math.random()*2000))+'ms'
-})
-   })
+    'animationDuration': (Math.floor(Math.random()*2000)+2000)+'ms',
+    'animationDelay': (Math.floor(Math.random()*2000))+'ms'
+  })
+  })
 
 $("#form").validate({
   rules: {
@@ -27,8 +35,13 @@ $("#form").validate({
   },
   errorPlacement: function(error, element) {
     return true;
+  },
+  submitHandler: function(form) {
+    $('.popup').removeClass('hidden')
   }
 })
 })
 
-
+$('.popup__btn-close').click(function () {
+  $('.popup').addClass('hidden');
+})
